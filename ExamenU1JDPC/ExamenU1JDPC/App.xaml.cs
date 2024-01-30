145069a5c76bcfa1aebd2665f71a3fe8d173acc7
LDVP5049 <luismaster752@gmail.com>
2024-01-30 09:10:23 -0700

    Agregar archivos de proyecto.

---
+++ ExamenU1JDPC/ExamenU1JDPC/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using ExamenU1JDPC.Vistas;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ExamenU1JDPC
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new examenVista();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ ExamenU1JDPC/ExamenU1JDPC/Vistas/examenVista.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ExamenU1JDPC.Vistas
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class examenVista : ContentPage
+    {
+        public examenVista()
+        {
+            InitializeComponent();
+        }
+    }
+}
